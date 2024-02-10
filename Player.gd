f51c8ff4890c30ba4972c32ae9f4a01af1d49eef
BaritoneBeard <nickalmeder@yahoo.com>
2024-02-10 16:32:14 -0500

    initial commit, guy in room as always

---
+++ Player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+var move_direction
+
+# @onready var sprite = get_node("HappyBoo/AnimationPlayer")
+
+func _physics_process(delta):
+	_update_move_direction()
+	_apply_movement()
+
+func _update_move_direction():
+	move_direction = Input.get_axis("ui_left", "ui_right")
+
+func _apply_movement():
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * SPEED
+		
+	move_and_slide()
+	
+	if velocity.length() > 0.0:
+		$HappyBoo.play_walk_animation()
+	else:
+		$HappyBoo.play_idle_animation()
