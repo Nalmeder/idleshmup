0edc3fdd8b668a32ff5164ef4b81b1172936a0de
BaritoneBeard <nickalmeder@yahoo.com>
2025-01-10 14:41:45 -0500

    added animation hurt effects on green slime only

--- Scripts/health_component.gd
+++ Scripts/health_component.gd
@@ -2,12 +2,15 @@ extends Node2D
 
 @export var max_health: int = 3  # default value
 var current_health: int          # prototype value
+signal entity_hit
 
 func _ready():
 	current_health = max_health  # Initialize health
+	
 
 func reduce_health(amount):
 	current_health -= amount
+	emit_signal('entity_hit')
 	print("Health reduced by ", amount, ". Current health: ", current_health)
 	
 	if current_health <= 0:
--- characters/slime/slime.gd
+++ characters/slime/slime.gd
@@ -8,3 +8,7 @@ func play_walk():
 func play_hurt():
 	%AnimationPlayer.play("hurt")
 	%AnimationPlayer.queue("walk")
+
+
+func _on_health_component_entity_hit():
+	play_hurt()
