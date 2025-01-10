ffa00a4a930f2d922413dd0912ff897ba6b6fd1e
BaritoneBeard <nickalmeder@yahoo.com>
2025-01-10 12:40:43 -0500

    cleaned up code, moved hurt logic to its own component

---
+++ on_damage_component.gd
@@ -0,0 +1,24 @@
+extends Node2D
+
+func check_frames(i_frame_length: int):
+	if %"I-Frames".time_left <= 0.05:
+		adjust_opacity(1.0)
+		%"I-Frames".stop()
+			
+	if !(%"I-Frames".is_stopped()):  # TODO: State Machine in the future
+		flicker(i_frame_length)
+	
+
+func flicker(delay: int):
+	if Engine.get_frames_drawn() % delay < int((delay/2)):
+		adjust_opacity(0.5)
+	else:
+		adjust_opacity(1.0)
+		
+		
+func adjust_opacity(num):
+	get_parent().modulate = Color(num, num ,num)
+	
+func start_i_frames():
+	%"I-Frames".start()
+	
