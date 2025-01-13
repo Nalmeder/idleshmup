ef9fc5e2c40f8513dc73e8f17940028574636710
BaritoneBeard <nickalmeder@yahoo.com>
2025-01-13 11:15:31 -0500

    added random enemy spawn locations

---
+++ game_controller.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const GREEN_SLIME = preload("res://characters/slime/Green_Slime.tscn")
+
+func _ready():
+	spawn_mob(GREEN_SLIME)
+	spawn_mob(GREEN_SLIME)
+
+func spawn_mob(mob):
+	var new_mob = mob.instantiate() 
+	%Spawn_Path.progress_ratio = randf()
+	new_mob.global_position = %Spawn_Path.global_position
+	add_child(new_mob)
+	
+	
